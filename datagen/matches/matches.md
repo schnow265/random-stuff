4ffb6d4dd357deb0fb2eac24f024fb0c56630b52
schnow265 <tamschnow265@proton.me>
2023-04-20 17:12:01 +0200

    re:did the layout

---
+++ datagen/matches/matches.md
@@ -0,0 +1,35 @@
+# Time for big Numbers
+
+> Well... *How fast* is it?
+
+## My Computer
+
+| Numbers | Time | Players |
+| ----- | ----- | ----- |
+| 30 | 0.0002636909 s | 3 |
+| 100'000 | 0.97 s | 3 |
+| 1 Million | 10.89 s | 3 |
+
+### REALLY BIG Numbers
+
+| Numbers | Time | Players |
+| --- | --- | --- |
+| 10 Million | 117.4 s | 3 |
+
+## Low-End PC's
+
+| Numbers | Time | Players |
+| --- | --- | --- |
+| | | |
+
+## GHCodespaces*
+
+| Numbers | Time | Players |
+| --- | --- | --- |
+| 30 | 0.03 | 3 |
+
+
+
+
+
+*) *Graphing disabled*
---
+++ datagen/matches/matches.py
@@ -0,0 +1,111 @@
+import random
+import os
+import csv
+import matplotlib.pyplot as plt
+import time
+import configparser
+
+# Konfiguration
+config = configparser.ConfigParser()
+config.read('matches.conf')
+
+endliner = config.getboolean('FUN', 'endliner', fallback=False)
+
+# Input Variables
+num_players = config.getint('INPUT', 'players', fallback=3)
+num_simulations = config.getint('INPUT', 'games', fallback=30)
+
+# Working with se Data
+cvs_verdict = config.getboolean('DATA', 'removeCSV', fallback=False)
+result_file = config.get('DATA', 'csv_file', fallback='results.csv')
+
+# Debug Configs
+tdp = config['DEBUG'].getint('time_decimal_places', fallback=2)
+verbose_output = config.getboolean('DEBUG', 'verbose_output', fallback=False)
+runtime_out = config.getboolean('DEBUG', 'runtime', fallback=True)
+
+# CLEANUP
+algclear = config.getboolean('QUALITY', 'clear_alg', fallback=False)
+
+linclear = config.getboolean('QUALITY', 'clear_screen_linux', fallback=False)
+winclear = config.getboolean('QUALITY', 'clear_screen_win', fallback=False)
+
+
+# Simulation
+start_time = time.time()
+results = []
+for i in range(num_simulations):
+    sticks = ["lang"] * num_players
+    sticks[random.randint(0, num_players - 1)] = "kurz"
+    random.shuffle(sticks)
+    loser = sticks.index("kurz") + 1
+    results.append(loser)
+    if verbose_output:
+        print(f"Round {i+1} lost Player {loser}.")
+
+if algclear:
+    if linclear:
+        os.system('clear')
+    elif winclear:
+        os.system('cls')
+    else:
+        pass
+
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+if runtime_out:
+    print(f"Runtime: {elapsed_time:.{tdp}} seconds")
+
+# Writing into CSV
+with open(result_file, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Result"])
+    for loser in results:
+        writer.writerow([loser])
+
+if config.getboolean('DATA', 'genplot', fallback=False):
+    # Group and Count
+    with open(result_file, "r") as f:
+        reader = csv.reader(f)
+        next(reader)  # Skip header
+        results_dict = {}
+        for row in reader:
+            result = int(row[0])
+            if result in results_dict:
+                results_dict[result] += 1
+            else:
+                results_dict[result] = 1
+
+    # Plot Creation
+    counts = []
+    for i in range(1, num_players+1):
+        if i in results_dict:
+            counts.append(results_dict[i])
+        else:
+            counts.append(0)
+    plt.bar(range(1, num_players+1), counts)
+    plt.title("Match of Matches")
+
+    # Hightest Value
+    max_value = max(counts)
+    max_index = counts.index(max_value)
+    text = f"Hightest Value: {max_value}, Player {max_index+1}"
+    plt.text(max_index+0.5, max_value*0.9, text, ha="center", va="center")
+
+    plt.xticks(range(1, num_players+1), [f"Player {i}" for i in range(1, num_players+1)])
+    plt.ylabel("Times the short match has been chosen.")
+    plt.show()
+
+if algclear:
+    if linclear:
+        os.system('clear')
+    elif winclear:
+        os.system('cls')
+    else:
+        pass
+
+if cvs_verdict:
+    os.remove(result_file)
+    print("CSV Removed!")
